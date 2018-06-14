f18e9863f9f8934c667cde978e11d963e1beed46
William Myers <williamrmyers@gmail.com>
2018-06-13 17:48:30 -0700

    Added scrollig to bottom of messenger.

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+let scrollToBottom = () => {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+};
+
 socket.on('connect', () => {
   console.log('Connected to server');
 });
@@ -20,6 +36,7 @@ socket.on(`newMessage`, (message) => {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 
 
   // console.log(`recieved new message!`, message);
@@ -40,6 +57,7 @@ socket.on('newLocationMessage', (message) => {
   });
 
   $('#messages').append(html);
+  scrollToBottom();
 
   // let li = $('<li></li>');
   // let a = $('<a target="_blank" >My Current Location</a>');
